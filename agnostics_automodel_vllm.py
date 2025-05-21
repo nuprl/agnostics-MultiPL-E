af5be17dc37af250af4593afc5b260b351c2845e
Aleksander Boruch-Gruszecki <research@abgru.me>
2025-05-21 04:16:15 -0400

    Add a Qwen3 flag to agnostics_automodel_vllm

--- agnostics_automodel_vllm.py
+++ agnostics_automodel_vllm.py
@@ -8,7 +8,15 @@ import torch
 
 
 class VLLM:
-    def __init__(self, name, revision, tokenizer_name=None, tokenizer_revision=None, num_gpus=1):
+    def __init__(
+        self,
+        name,
+        revision,
+        tokenizer_name,
+        tokenizer_revision,
+        num_gpus = 1,
+        use_qwen3_nothink_sysprompt = False,
+    ):
         dtype = "float16"
         if torch.cuda.is_bf16_supported():
             dtype = "bfloat16"
@@ -23,14 +31,31 @@ class VLLM:
             tensor_parallel_size=num_gpus,
             gpu_memory_utilization=0.95,
         )
+        self.use_qwen3_nothink_sysprompt = use_qwen3_nothink_sysprompt
 
     def completions(
         self, prompts: List[str], max_tokens: int, temperature: float, top_p, stop
     ):
-        prompts = [prompt.strip() for prompt in prompts]
-        params = SamplingParams(temperature=temperature,
-                                top_p=top_p, max_tokens=max_tokens, stop=stop)
-        outputs = self.model.generate(prompts, params, use_tqdm=False)
+        if not self.use_qwen3_nothink_sysprompt:
+            prompts = [prompt.strip() for prompt in prompts]
+        else:
+            new_prompts = []
+            for p in prompts:
+                chat = []
+                chat.append({ "role": "system", "content": "/nothink" })
+                chat.append({ "role": "user", "content": p })
+                new_prompts.append(chat)
+            prompts = new_prompts
+        params = SamplingParams(
+            temperature=temperature,
+            top_p=top_p,
+            max_tokens=max_tokens,
+            stop=stop,
+        )
+        if not self.use_qwen3_nothink_sysprompt:
+            outputs = self.model.generate(prompts, params, use_tqdm=False)
+        else:
+            outputs = self.model.chat(prompts, params, use_tqdm=False)
         outputs = [o.outputs[0] for o in outputs]
         return [
             (
@@ -47,6 +72,7 @@ def automodel_partial_arg_parser():
     args.add_argument("--tokenizer_name", type=str)
     args.add_argument("--tokenizer_revision", type=str)
     args.add_argument("--name-override", type=str)
+    args.add_argument("--use-qwen3-nothink-sysprompt", action="store_true")
     args.add_argument("--num-gpus", type=int, default=1)
     return args
 
@@ -67,7 +93,8 @@ def main():
     args = automodel_partial_arg_parser()
     args = args.parse_args()
     model = VLLM(args.name, args.revision, args.tokenizer_name,
-                 args.tokenizer_revision, args.num_gpus)
+                 args.tokenizer_revision, args.num_gpus,
+                 args.use_qwen3_nothink_sysprompt)
     name = do_name_override(args)
     make_main(args, name, model.completions)
 
